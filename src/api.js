89e8891522a3b53109678c413f8ce26bf81a1962
Matt Morris <mcm0027@yahoo.com>
2015-11-30 15:05:37 -0600

    finish API services and testing for them

--- src/api.js
+++ src/api.js
@@ -18,141 +18,45 @@ ccApi.config(function ($routeProvider) {
   
   .when ('/countries/:country/:code', {
     templateUrl: 'pages/country.html',
-  //  controller: 'searchController'
+    controller: 'searchController'
   });
 });
 
-ccApi.factory('getCountries', [ '$http', '$q', function ($http, $q) { 
+  ccApi.controller('listController', ['$scope', 'getCountries', function($scope, getCountries) {
+    
+    getCountries.search().then(passCountries);
 
-  var service = {}
+    function passCountries(data) {
+      $scope.response = data.geonames;
+      console.log($scope.response);
+    }
+  }]);
+
+ccApi.controller('searchController', ['$scope', '$routeParams', 'countrySearch', 'countryInfo', 'neighborCountries', function($scope, $routeParams, countrySearch, countryInfo, neighborCountries) {
   
-  service.search = function() {
-    var deferred = $q.defer();
-    $http.get("http://api.geonames.org/countryInfoJSON?username=mcm0027")
-      .success(function(data) {
-        deferred.resolve(data);
-      });
-    return deferred.promise;
+  $scope.country = $routeParams.country;
+  $scope.code = $routeParams.code;
+
+  countrySearch.search($scope.code).then(passCountry);
+
+  function passCountry(data) {
+    $scope.results = data.geonames[0];
   }
-  return service;
+
+  countryInfo.search($scope.code).then(passCountryInfo);
+
+  function passCountryInfo(data) {
+    console.log(data.geonames[0]);
+    $scope.infoResults = data.geonames[0];
+    console.log($scope.infoResults);
+  }
+  
+  neighborCountries.search($scope.code).then(passNeighbors);
+  
+  function passNeighbors(data) {
+    console.log(data.geonames);
+    $scope.resultsNeighbor = data.geonames;
+    console.log($scope.resultsNeighbor);
+  }
+  
 }]);
-  
-  
-  /*
-  function getCountries() {
-    return $http({
-      url: "http://api.geonames.org/countryInfoJSON?username=mcm0027",
-      cache: true
-
-    })
-
-    .then(function(result) {
-          return result;
-    })};
-    
-  }]);
-*/
-        
-  ccApi.controller('listController', ['$scope', '$http', function($scope, $http) {
-
-    $scope.getCountries = function() {
-      return $http({
-        url: "http://api.geonames.org/countryInfoJSON?username=mcm0027",
-        cache: true
-
-      })
-
-        .then(function(result) {
-        $scope.response = result.data.geonames;
-        console.log($scope.response);
-      })}();
-
-  }]);
-  /*
-  ccApi.controller('searchController', ['$http', '$scope', '$routeParams', '$log', function($http, $scope, $routeParams, $log) {
-
-    
-      $scope.searchCountries = function() {
-  
-        var code = "AE";
-        var country = "United Arab Emirates";
-        
-        $scope.country = $routeParams.country || country;
-        $scope.code = $routeParams.code || code;
-        
-        console.log($scope.code);
-                                        
-
-        var url = "http://api.geonames.org/searchJSON?username=mcm0027&style=LONG&maxRows=1&country=" + $scope.code;
-
-
-        $http({
-          url: url
-
-        })
-        .then(function(result) {
-         console.log(result.data.geonames[0]);
-          $scope.results = result.data.geonames[0];
-          console.log($scope.results);          
-          return $scope.results;
-          console.log($scope.results);
-        })
-
-      }();
-
-    
-    var getCountryInfo = function() {
-      
-      $scope.code = $routeParams.code;
-      
-      var url = "http://api.geonames.org/countryInfoJSON?username=mcm0027";
-      
-      var request = {
-          country: $scope.code
-        };
-      $http({
-        url: url,
-        cache: true,
-        params: request
-
-      })
-      .then(function(result) {
-        console.log(result.data.geonames[0])
-        $scope.infoResults = result.data.geonames[0];
-        return $scope.infoResults;
-        console.log($scope.infoResults);
-      })
-
-    };
-  
-    getCountryInfo();
-  
-
-
-      var neighborCountries = function() {
-        $scope.code = $routeParams.code;
-
-
-        var url = "http://api.geonames.org/neighboursJSON?username=mcm0027";
-        var request = {
-          country: $scope.code
-        };
-
-        $http({
-          url: url,
-          params: request
-
-        })
-        .then(function(result) {
-         console.log(result.data.geonames);
-          $scope.resultsNeighbor = result.data.geonames;
-          return $scope.resultsNeighbor;
-          console.log($scope.resultsNeighbor);
-        })
-
-      };
-
-      neighborCountries();
-    
-   }]);
-*/
---
+++ src/dataService.js
@@ -0,0 +1,60 @@
+ccApi.factory('getCountries', [ '$http', '$q', function ($http, $q) { 
+
+  var service = {}
+
+  service.search = function() {
+    var deferred = $q.defer();
+    $http.get("http://api.geonames.org/countryInfoJSON?username=mcm0027")
+      .success(function(data) {
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  }
+  return service;
+}]);
+
+ccApi.factory('countrySearch', [ '$http', '$q', function ($http, $q) { 
+
+  var service = {}
+
+  service.search = function(code) {
+    var deferred = $q.defer();
+    $http.get("http://api.geonames.org/searchJSON?username=mcm0027&style=LONG&maxRows=1&country=" + code)
+      .success(function(data) {
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  }
+  return service;
+}]);
+
+ccApi.factory('countryInfo', [ '$http', '$q', function ($http, $q) { 
+
+  var service = {}
+
+  service.search = function(code) {
+    var deferred = $q.defer();
+    $http.get("http://api.geonames.org/countryInfoJSON?username=mcm0027&country=" + code)
+      .success(function(data) {
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  }
+  return service;
+}]);
+
+ccApi.factory('neighborCountries', [ '$http', '$q', function ($http, $q) { 
+
+  var service = {}
+
+  service.search = function(code) {
+    var deferred = $q.defer();
+    $http.get("http://api.geonames.org/neighboursJSON?username=mcm0027&country=" + code)
+      .success(function(data) {
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  }
+  return service;
+}]);
+
